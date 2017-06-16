7f7b5affe2adcdf385f645e7c956da1d10a84b24
Jorge Sánchez de Molina Martínez <jorsmm@gmail.com>
2017-06-16 23:34:22 +0200

    Initial commit

---
+++ ac_salon_off.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/Users/casa/broadlink_rm3/broadlink_rm3.sh AC_SALON_OFF >> /tmp/kk
---
+++ ac_salon_on_23.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/Users/casa/broadlink_rm3/broadlink_rm3.sh AC_SALON_ON_23 >> /tmp/kk
+/Users/casa/broadlink_rm3/broadlink_rm3.sh AC_SALON_COMFORT >> /tmp/kk
